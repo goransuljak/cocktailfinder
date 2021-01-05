416be2a298330e2c479c65b8721e1e1f049195aa
Goran <goranfrance@hotmail.com>
2021-01-05 20:57:31 +0000

    CHANGES

---
+++ main.js
@@ -0,0 +1,12 @@
+// DOM ELEMENTS THAT I NEED
+
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+
+const cocktailsEl = document.getElementById('cocktails');
+const resultText = document.getElementById('result-text');
+const singleCocktail = document.getElementById('single-cocktail');
+
+
+// EVENT LISTENERS
