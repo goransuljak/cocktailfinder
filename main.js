da5b45f75a78b4a50973da5b79b2fc4124071e7b
Goran <goranfrance@hotmail.com>
2021-01-05 21:19:34 +0000

    saf

--- main.js
+++ main.js
@@ -8,5 +8,29 @@ const cocktailsEl = document.getElementById('cocktails');
 const resultText = document.getElementById('result-text');
 const singleCocktail = document.getElementById('single-cocktail');
 
+// FUNCTIONS
+function searchCocktail(event) {
+event.preventDefault();
+
+// clear single cocktail div
+singleCocktail.innerHTML = '';
+
+// get search value
+const term = search.value;
+
+// check for word
+if(term.trim()) {
+    fetch(` https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        resultText.innerHTML = `<p>Results for <i>"${term}</i>"</p>`;
+    })
+} else {
+    alert('Enter search value');
+}
+}
 
 // EVENT LISTENERS
+
+submit.addEventListener('submit', searchCocktail);
