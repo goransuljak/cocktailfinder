c7a3f57e1eeee47952513e50fa5984d7e3412564
Goran <goranfrance@hotmail.com>
2021-01-06 23:20:30 +0000

    work for today

--- main.js
+++ main.js
@@ -25,12 +25,75 @@ if(term.trim()) {
     .then(data => {
         console.log(data);
         resultText.innerHTML = `<p>Results for <i>"${term}</i>"</p>`;
-    })
-} else {
-    resultText.innerHTML = `<p><i>enter valid search term</i></p>`;
+        if(data.drinks === null) {
+            resultText.innerHTML = ``;
+        } else {
+            cocktailsEl.innerHTML = data.drinks.map(drink => `
+            <div class="cocktails">
+            <img src="${drink.strDrinkThumb}" />
+            <div class="cocktails-info" drink-id="${drink.idDrink}">
+            <p>${drink.strDrink}</p>
+            <p>(${drink.strGlass})</p>
+            </div>
+            </div>
+            `).join('');
+        }
+    });
+    search.value = "";
+} 
 }
+// fetch drink by ID
+function getDrinkByID(drinkID) {
+    fetch(` https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`)
+    .then(res => res.json())
+    .then(data => {
+      const drink = data.drinks[0];
+      
+      addDrinkToDOM(drink);
+      
+    });
+}
+// add drink to DOM 
+function addDrinkToDOM(drink) {
+    const ing = [];
+    for(let i = 1; i < 10; i++) {
+      if(drink[`strIngredient${i}`]) {
+       ing.push(`${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`);
+      } else {
+        break;
+      }
+    }
+
+    singleCocktail.innerHTML = `
+    <div class="single-cocktail">
+    <h2>${drink.strDrink}</h2>
+    <p>${drink.strGlass} ~ ${drink.strCategory}</p>
+    <img src="${drink.strDrinkThumb}" />
+    <div class="main">
+    <p>${drink.strInstructions}</p>
+    <h3>Ingredients</h3>
+    <ul>
+    ${ing.map(ing => `<li>${ing}</li>`).join('')}
+    </ul>
+    </div>
+    </div>
+    `;
 }
 
 // EVENT LISTENERS
 
 submit.addEventListener('submit', searchCocktail);
+
+cocktailsEl.addEventListener('click', e => {
+    const drinkInfo = e.path.find(item => {
+        if(item.classList) {
+            return item.classList.contains('cocktails-info');
+        } else {
+            return false;
+        }
+    });
+    if(drinkInfo) {
+        const drinkId = drinkInfo.getAttribute('drink-id');
+        getDrinkByID(drinkId);
+    }
+});
